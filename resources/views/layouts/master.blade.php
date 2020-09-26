3eab6352e4b4ebd0ab9f6e7511468e7d2c55ff64
neveleneve <pandupandu813@gmail.com>
2020-09-26 19:54:53 +0700

    test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -201,8 +201,6 @@
     <script src="{{url('admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{url('admin/plugins/chart.js/Chart.min.js')}}"></script>
     <script src="{{url('admin/plugins/sparklines/sparkline.js')}}"></script>
-    <script src="{{url('admin/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-    <script src="{{url('admin/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
     <script src="{{url('admin/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
     <script src="{{url('admin/plugins/moment/moment.min.js')}}"></script>
     <script src="{{url('admin/plugins/daterangepicker/daterangepicker.js')}}"></script>
@@ -210,7 +208,6 @@
     <script src="{{url('admin/plugins/summernote/summernote-bs4.min.js')}}"></script>
     <script src="{{url('admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
     <script src="{{url('admin/dist/js/adminlte.js')}}"></script>
-    <script src="{{url('admin/dist/js/pages/dashboard.js')}}"></script>
     <script src="{{url('admin/dist/js/demo.js')}}"></script>    
 </body>
 
