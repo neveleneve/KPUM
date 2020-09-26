665383ad1abf89f0d4f5d8fc2942193b2c3a5164
neveleneve <pandupandu813@gmail.com>
2020-09-26 19:36:58 +0700

    mana

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,16 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @yield('title')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="shortcut icon" href="{{asset('admin/dist/img/kpum.png')}}" type="image/x-icon">
-    <link type="text/css" rel="stylesheet" href="{{asset('admin/plugins/fontawesome-free/css/all.min.css')}}">
+    <link type="text/css" rel="shortcut icon" href="{{url('admin/dist/img/kpum.png')}}" type="image/x-icon">
+    <link type="text/css" rel="stylesheet" href="{{url('admin/plugins/fontawesome-free/css/all.min.css')}}">
     <link type="text/css" rel="stylesheet"
-        href="{{asset('admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
-    <link type="text/css" rel="stylesheet" href="{{asset('admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-    <link type="text/css" rel="stylesheet" href="{{asset('admin/plugins/jqvmap/jqvmap.min.css')}}">
-    <link type="text/css" rel="stylesheet" href="{{asset('admin/dist/css/adminlte.min.css')}}">
-    <link type="text/css"rel="stylesheet" href="{{asset('admin/plugins/overlayScrollbars/css/overlayScrollbars.min.css')}}">
-    <link type="text/css" rel="stylesheet" href="{{asset('admin/plugins/daterangepicker/daterangepicker.css')}}">
-    <link type="text/css" rel="stylesheet" href="{{asset('admin/plugins/summernote/summernote-bs4.css')}}">
+        href="{{url('admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{url('admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{url('admin/plugins/jqvmap/jqvmap.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{url('admin/dist/css/adminlte.min.css')}}">
+    <link type="text/css"rel="stylesheet" href="{{url('admin/plugins/overlayScrollbars/css/overlayScrollbars.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{url('admin/plugins/daterangepicker/daterangepicker.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{url('admin/plugins/summernote/summernote-bs4.css')}}">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 
@@ -192,16 +192,16 @@
         </aside>
         @yield('content')
     </div>
-    <script src="{{asset('admin/plugins/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
-    <script src="{{asset('admin/dist/js/adminlte.js')}}"></script>
-    <script src="{{asset('admin/plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
-    <script src="{{asset('admin/plugins/raphael/raphael.min.js')}}"></script>
-    <script src="{{asset('admin/plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
-    <script src="{{asset('admin/plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
-    <script src="{{asset('admin/plugins/chart.js/Chart.min.js')}}"></script>
-    <script src="{{asset('admin/dist/js/pages/dashboard2.js')}}"></script>
+    <script src="{{url('admin/plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{url('admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{url('admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+    <script src="{{url('admin/dist/js/adminlte.js')}}"></script>
+    <script src="{{url('admin/plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
+    <script src="{{url('admin/plugins/raphael/raphael.min.js')}}"></script>
+    <script src="{{url('admin/plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
+    <script src="{{url('admin/plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
+    <script src="{{url('admin/plugins/chart.js/Chart.min.js')}}"></script>
+    <script src="{{url('admin/dist/js/pages/dashboard2.js')}}"></script>
 </body>
 
 </html>
