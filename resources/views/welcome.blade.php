e22d8f739217fac73689deaaa4b7bc99fc936a16
neveleneve <pandupandu813@gmail.com>
2020-09-26 19:17:48 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,6 +111,13 @@
         </div>
     </section>
 </div>
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.0.5
+    </div>
+</footer>
 {{-- Modal --}}
 <div class="modal fade" id="login-modal">
     <div class="modal-dialog">
